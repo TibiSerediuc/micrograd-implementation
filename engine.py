ad7c81ae84fe2f40a2298fae53cb58ea9be19cef
TibiSerediuc <tiberiu.serediuc@gmail.com>
2025-06-04 00:00:31 +0300

    Added addition and multiplication operations for class Value

--- engine.py
+++ engine.py
@@ -6,5 +6,12 @@ class Value:
     def __repr__(self):
         return f"Value(data = {self.data})"
 
+    def __add__(self, other):
+        return Value(self.data + other.data)
+
+    def __mul__(self, other):
+        return Value(self.data * other.data)
+
 a = Value(2.0)
-print(a)
+b = Value(3.0)
+print(a + b)
